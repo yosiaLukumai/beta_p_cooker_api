6ad397cae68f334a4e8a37ccaba1b754e2ccbf35
yosiaLukumai <yosialukumai@gmail.com>
2025-06-29 20:05:39 +0300

    config

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,10 @@ const configuredPort = config.PROD_READY ? config.prod_port : config.port;
 
 app.listen(configuredPort, () => {
   if (config.PROD_READY) {
+    console.log("Production server running on port", config.prod_port);
     console.warn("Production server running on port", config.prod_port);
   } else {
+    console.log("Development server running on port", config.port);
     console.warn("Development server running on port", config.port);
   }
 });
